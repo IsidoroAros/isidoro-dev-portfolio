82a5d1d07d02209c63efd7c1c3bdd1abe025bc30
Isidoro Aróstegui <arostegui.isidoro@gmail.com>
2020-12-18 13:24:25 -0300

    Added projects miniatures

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+// General
+
+
+
+// Navbar
+
 const hamburger = document.querySelector('.hamburger-icon');
 const navList = document.querySelector('.navbar-list')
 const navBar = document.querySelector('.navbar-top');
@@ -6,3 +12,26 @@ hamburger.addEventListener('click', ()=>{
     navList.classList.toggle('active');    
     navBar.classList.toggle('active');    
 })
+
+// Card and projects
+
+const projectContainer = document.querySelector('#projects');
+const projectDetail = document.querySelector('.project-injection-view');
+
+
+
+projectContainer.addEventListener('click', mostrarProyecto);
+
+function mostrarProyecto(e) {
+    console.log('llego hasta aca')
+    if(e.target.classList.contains('project-card')){
+        console.log('Si lo tiene');
+        const proyecto = {
+            miniatura: document.querySelector('.project-card img').src,
+            project: document.querySelector('.project-card h2').textContent,
+            descripcion: document.querySelector('.project-card p').textContent,
+            id: document.querySelector('.project-card').getAttribute('data-id')
+        }
+        console.log(proyecto)
+    }
+}
