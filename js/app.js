d7e271f5d521109ff46282dfd55a2691b9efe0fd
Isidoro Aróstegui <arostegui.isidoro@gmail.com>
2020-12-22 19:00:39 -0300

    Added mobile transitions

--- js/app.js
+++ js/app.js
@@ -22,26 +22,59 @@ const projectInjectionView = document.querySelector('.project-injection-view');
 const closeButton = document.querySelector('.close-button');
 
 projectContainer.addEventListener('click', mostrarProyecto);
-closeButton.addEventListener('click', clearInjection)
+projectContainer.addEventListener('click', (e) => {
+    if(e.target.classList.contains('close-button') || e.target.parentNode.classList.contains('close-button')){
+        projectInjectionView.innerHTML = '';
+    }
+})
 
 function mostrarProyecto(e) {
-    clearInjection();
     if(e.target.parentNode.classList.contains("project-card") || e.target.parentNode.parentNode.classList.contains("project-card")){
-        
         const proyecto = {
             miniatura: document.querySelector('.project-card img').src,
             project: document.querySelector('.project-card h2').textContent,
             descripcion: document.querySelector('.project-card p').textContent,
             id: document.querySelector('.project-card').getAttribute('data-id')
         }
-        document.createElement('div').classList.add('close-button-container');
-        
-           
-
-    }
+        projectInjectionView.innerHTML = `
+        <div class="close-button-container">
+            <div class="close-button"><i class="fas fa-times" id="x-icon"></i></div>
+            </div>
+            
+            <div class="project-injection-cover">
+            <img src="${proyecto.miniatura}" alt="">
+            </div>
+            
+            <div class="project-injection-info">
+            <h1 class="project-injection-title">${proyecto.project}</h1>
+            <p class="project-injection-paragraph">${proyecto.descripcion}</p>
+            </div>
+            
+            <div class="project-buttons">
+            <button class="project-btn" id="github-button"><img src="img/github.svg" alt=""><a class="button-reference" href="#">Browse code</a></button>
+            <button class="project-btn"><img src="img/livestream.png" alt=""><a class="button-reference" href="#" id="preview-button">Watch live</a></button>
+            </div>
+            `
+        }
+        window.scrollBy(0,400);
 }
-
-function clearInjection() {
+    
+function clearInjection(e) {
     projectInjectionView.innerHTML = '';
 }
 
+// Animations
+
+window.addEventListener('scroll', createAnimations)
+const aboutMain = document.querySelector('.about-main')
+
+function createAnimations() {
+    if(window.scrollY >= aboutMain.getBoundingClientRect().top - 150){
+        aboutMain.classList.add('animate__animated', 'animate__fadeInUp')
+        aboutMain.style.display = 'block';
+    }
+    if(window.scrollY >= projectContainer.getBoundingClientRect().top - 150){
+        projectContainer.classList.add('animate__animated', 'animate__fadeInUp')
+        projectContainer.style.display = 'block';
+    }
+}
