f4969772d6bff5e84ccf6093ac7e42958713c92f
Isidoro <arostegui.isidoro@gmail.com>
2021-01-03 20:40:39 -0300

    Added footer

--- js/app.js
+++ js/app.js
@@ -82,20 +82,3 @@ function clearInjection() {
 // window.addEventListener('scroll', createAnimations)
 const aboutMain = document.querySelector('.about-main')
 
-// function createAnimations() {
-//     if(window.innerWidth >= 750){
-//         if(window.scrollY >= aboutMain.getBoundingClientRect().top - 200){
-//             aboutMain.classList.add('animate__animated', 'animate__fadeInUp')
-//         }
-//         if(window.scrollY >= projectContainer.getBoundingClientRect().top - 200){
-//             projectContainer.classList.add('animate__animated', 'animate__fadeInUp')
-//         }
-//     }else{
-//         if(window.scrollY >= aboutMain.getBoundingClientRect().top - 300){
-//             aboutMain.classList.add('animate__animated', 'animate__fadeInUp')
-//         }
-//         if(window.scrollY >= projectContainer.getBoundingClientRect().top - 300){
-//             projectContainer.classList.add('animate__animated', 'animate__fadeInUp')
-//         }
-//     }
-// }
